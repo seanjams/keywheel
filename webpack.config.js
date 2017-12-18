fb92563738a7f7779f2d4595b5afd776033b8060
Sean OReilly <seanvoreilly2@gmail.com>
2017-12-18 12:21:02 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: 'components/entry.jsx'),
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
