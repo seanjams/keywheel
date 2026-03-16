ef2e11b13c5602ed194ed273e59dedc501756593
Sean O'Reilly <seanvoreilly2@gmail.com>
2026-03-16 00:55:38 -0700

    Deployment Commit

--- src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { App } from "./app";
-import { AppStore } from "../store/state";
-import { useMemo } from "react";
-
-export const Container: React.FC = () => {
-    const appStore = useMemo(() => new AppStore(), []);
-    return <App appStore={appStore} />;
-};
--- src/util/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./colors";
-export * from "./consts";
-export * from "./helpers";
-export * from "./vertices/chordCube";
-export * from "./vertices/keyCube";
